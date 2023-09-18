ef9fd25e6c18236d2b12ac3baed9d2c5918ab6bf
Konstantin Zavgorodniy <zavkostya@gmail.com>
2023-09-18 18:33:42 -0400

    db extension

--- init-league.py
+++ init-league.py
@@ -5,13 +5,11 @@ from os import makedirs
 
 
 def init_db(league_id):
-    # TODO check if league exists, if not create it
-    # TODO create a db with the league data, store it in the folder espn-data/league_id/league.db
     try:
         makedirs(f'espn-data/{league_id}')
     except FileExistsError:
         pass
-    conn = sqlite3.connect(f'espn-data/{league_id}/league.sqlite')
+    conn = sqlite3.connect(f'espn-data/{league_id}/league.db')
     cursor = conn.cursor()
 
     scoring_table = {value: "REAL" for value in schemas.espn_to_sqlite_names.values()}
@@ -22,6 +20,7 @@ def init_db(league_id):
     cursor.execute(get_create_table_command('defencemen_stats', schemas.sqlite_defencemen_stats_table))
     cursor.execute(get_create_table_command('goalie_stats', schemas.sqlite_goalies_stats_table))
     conn.commit()
+    conn.close()
 
 
 def get_create_table_command(table_name, table_schema):
@@ -53,16 +52,26 @@ def get_inputs():
     print('If a category is not used, enter 0 or leave it empty.')
     print('Make sure to align it with league setting on ESPN as some stat values are '
           '"products of other stats" and dont have own scoring value.')
-    # TODO uncomment this
-    # for key, value in schemas.espn_to_sqlite_names.items():
-    #     league['scoring'][value] = input_detect(f'{value:<5} {schemas.sqlite_column_descriptions[value]:<30}: ')
+
+    for key, value in schemas.espn_to_sqlite_names.items():
+        league['scoring'][value] = input_detect(f'{value:<5} {schemas.sqlite_column_descriptions[value]:<30}: ')
 
     return league
+
+
+def update_league_info(league):
+    conn = sqlite3.connect(f'espn-data/{league["league_id"]}/league.db')
+    cursor = conn.cursor()
+    args = ", ".join([str(value) for value in league["scoring"].values()])
+    cursor.execute(f'INSERT INTO scoring VALUES ({args});')
+    conn.commit()
+    conn.close()
 
 
 def main():
     data = get_inputs()
     init_db(data['league_id'])
+    update_league_info(data)
 
 
 if __name__ == '__main__':
