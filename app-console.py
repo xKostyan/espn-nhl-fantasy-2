9a836b64ac22e17a22ed70625000315c5dc60cef
Konstantin Zavgorodniy <zavkostya@gmail.com>
2024-07-21 18:28:03 -0400

    init work

--- app-console.py
+++ app-console.py
@@ -1,5 +1,6 @@
 from src import get_players_draft_data as get_players_draft_data
 from src import init_league as init_league
+from src import player_stats_visualiser as player_stats_visualiser
 import os
 
 def option_one():
@@ -42,8 +43,9 @@ def league_interactions_menu(league_id):
     print("\nSelect an option:")
     print("1. Update selected league data")
     print("2. Refresh leagues authentication")
-    print("3. Players prices adjustment")
-    print("4. Quit")
+    print("3. Player stats visualiser")
+    print("4. Players prices adjustment")
+    print("5. Quit")
     choice = input("\n:")
     match choice:
         case '1':
@@ -54,8 +56,10 @@ def league_interactions_menu(league_id):
             init_league.save_league_auth(auth)
             league_interactions_menu(league_id)
         case '3':
-            print("Not implemented yet.")
+            player_stats_visualiser.main(league_id)
         case '4':
+            print("Not implemented yet.")
+        case '5':
             print("Goodbye!")
             exit(0)
         case _:
